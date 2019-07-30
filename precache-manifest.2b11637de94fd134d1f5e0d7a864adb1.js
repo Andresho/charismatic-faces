91655f9b9022a812eb05239d6b903c139e18dc71
André Hoffmann <andre8hoffmann@gmail.com>
2019-07-30 16:10:38 -0300

    Updates

---
+++ precache-manifest.2b11637de94fd134d1f5e0d7a864adb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcfef14964df40f09a7770bd03ae3eb8",
+    "url": "/charismatic-faces/index.html"
+  },
+  {
+    "revision": "5dee7a83f933a0f90911",
+    "url": "/charismatic-faces/static/css/main.4d9f8cdb.chunk.css"
+  },
+  {
+    "revision": "3a3ffa65b27843b6ede0",
+    "url": "/charismatic-faces/static/js/2.43fa98a8.chunk.js"
+  },
+  {
+    "revision": "5dee7a83f933a0f90911",
+    "url": "/charismatic-faces/static/js/main.5eee0b89.chunk.js"
+  },
+  {
+    "revision": "96475a1ae65afd006a3c",
+    "url": "/charismatic-faces/static/js/runtime~main.449b292b.js"
+  },
+  {
+    "revision": "2b9ec60209a7f1c4fe5d62c1df593069",
+    "url": "/charismatic-faces/static/media/charismaticFace.2b9ec602.png"
+  }
+]);
